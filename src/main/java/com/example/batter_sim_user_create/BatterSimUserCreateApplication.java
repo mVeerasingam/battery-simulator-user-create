41e3b3889fbf6e7a2ca98833e019ef74f7542213
Mark Veerasingam <markveer70@gmail.com>
2025-01-01 20:26:26 +0000

    first commit

---
+++ src/main/java/com/example/batter_sim_user_create/BatterSimUserCreateApplication.java
@@ -0,0 +1,13 @@
+package com.example.batter_sim_user_create;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatterSimUserCreateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatterSimUserCreateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/batter_sim_user_create/BatterSimUserCreateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batter_sim_user_create;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatterSimUserCreateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
