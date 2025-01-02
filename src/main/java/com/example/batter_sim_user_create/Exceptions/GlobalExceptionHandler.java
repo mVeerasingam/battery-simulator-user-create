c590cb912199adcddb2a27f3b561534aea569469
Mark Veerasingam <markveer70@gmail.com>
2025-01-02 20:19:21 +0000

    Added ConstraintViolationException

--- src/main/java/com/example/batter_sim_user_create/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/batter_sim_user_create/Exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.batter_sim_user_create.Exceptions;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,6 +24,19 @@ public class GlobalExceptionHandler {
             String message = error.getDefaultMessage();
             errors.put(fieldName, message);
         });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    // ConstraintViolationException Handler
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, String>> handleConstraintViolationException(ConstraintViolationException ex) {
+        Map<String, String> errors = new HashMap<>();
+        Set<ConstraintViolation<?>> violations = ex.getConstraintViolations();
+        for (ConstraintViolation<?> violation : violations) {
+            String fieldName = violation.getPropertyPath().toString();
+            String message = violation.getMessage();
+            errors.put(fieldName, message);
+        }
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
